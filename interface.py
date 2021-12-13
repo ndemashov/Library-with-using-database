a725103f67cb0efeeaa539e8f89f5217b598d72f
Anton Klykov <antonklykov01@mail.ru>
2021-12-13 13:21:57 +0300

    Something change

--- interface.py
+++ interface.py
@@ -22,26 +22,18 @@ class Main(tk.Frame):
                                   compound=tk.TOP)
         btn_delete_db.pack(side=tk.LEFT)
 
+        btn_connect = tk.Button(toolbar, text='Connect', command=self.connect, bg='#d7d8e0', bd=0,
+                                compound=tk.TOP)
+        btn_connect.pack(side=tk.LEFT)
+
         btn_ct = tk.Button(toolbar, text='Create table', command=self.create_table, bg='#d7d8e0', bd=0,
                                   compound=tk.TOP)
         btn_ct.pack(side=tk.LEFT)
 
-        btn_connect = tk.Button(toolbar, text='Connect', command=self.connect, bg='#d7d8e0', bd=0,
-                           compound=tk.TOP)
-        btn_connect.pack(side=tk.LEFT)
-        '''
-        self.tree = ttk.Treeview(self, columns=('ID', 'description', 'costs', 'total'),
-                                 height=15, show='headings')
-        self.tree.column("ID", width=30, anchor=tk.CENTER)
-        self.tree.column("description", width=365, anchor=tk.CENTER)
-        self.tree.column("costs", width=150, anchor=tk.CENTER)
-        self.tree.column("total", width=100, anchor=tk.CENTER)
-        self.tree.heading("ID", text='ID')
-        self.tree.heading("description", text='Наименование')
-        self.tree.heading("costs", text='Статья дохода/расхода')
-        self.tree.heading("total", text='Сумма')
-        self.tree.pack()
-        '''
+        btn_add_book = tk.Button(toolbar, text='Add book', command=self.add_book, bg='#d7d8e0', bd=0,
+                                  compound=tk.TOP)
+        btn_add_book.pack(side=tk.LEFT)
+
 
     def add_db(self, name):
         self.db.create_db(name)
@@ -54,6 +46,9 @@ class Main(tk.Frame):
 
     def conn(self, name):
         self.db.connect(name)
+
+    def add_b(self, title, writing_year, author_name, author_surname, num_book):
+        self.db.query_add_book(title, writing_year, author_name, author_surname, num_book)
         
     def create_db(self):
         Create_db()
@@ -66,6 +61,9 @@ class Main(tk.Frame):
 
     def connect(self):
         Connect()
+
+    def add_book(self):
+        Add_book()
 
 class Template(tk.Toplevel):
      def __init__(self):
@@ -80,6 +78,7 @@ class Template(tk.Toplevel):
 
         self.grab_set()
         self.focus_set()         
+
 
 class Create_db(Template):
     def __init__(self):
@@ -121,52 +120,25 @@ class Delete_db(Template):
         btn_delete.bind('<Button-1>', lambda event: self.view.del_db(
             self.entry_name.get()))
 
-class DB:
+class Connect(Template):
     def __init__(self):
-        self.con = psycopg2.connect( 
-            user="postgres", 
-            password="123", 
-            host="127.0.0.1", 
-            port="5432"
-        )
-        self.cur = self.con.cursor()
-    def create_db(self, name):
-        self.con.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
-        query = "create database "+name+";"
-        self.cur.execute(query)
-        print("Created")
-        self.con.commit()
+        super().__init__()
+        self.init_create_table()
+        self.view = app
 
-    def delete_db(self, name):
-        self.con.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
-        query = "drop database "+name+";"
-        self.cur.execute(query)
-        print("Deleted")
-        self.con.commit()
-        
-    def connect(self, name):
-        self.con = psycopg2.connect( 
-            user="postgres",
-            database=name, 
-            password="123", 
-            host="127.0.0.1", 
-            port="5432"
-        )
-        print("Connected")
-        self.cur = self.con.cursor()
-    def close(self):
-        con.close()
-        
-    def create_table(self, name, structure):
+    def init_create_table(self):
+        self.title('Create table')
+
         label_name = tk.Label(self, text='Name:')
-        label_name.place(x=50, y=50)
-        label_structure = tk.Label(self, text='Structure:')
-        label_structure.place(x=50, y=50)
-        
-    def insert_data(self, description, costs, total):
-        self.c.execute('''INSERT INTO finance(description, costs, total) VALUES (?, ?, ?)''',
-                       (description, costs, total))
-        self.conn.commit()
+        label_name.place(x=50, y=20)
+
+        self.entry_name = ttk.Entry(self)
+        self.entry_name.place(x=200, y=20)
+
+        btn_connect = ttk.Button(self, text='Connect with db')
+        btn_connect.place(x=220, y=170)
+        btn_connect.bind('<Button-1>', lambda event: self.view.conn(
+            self.entry_name.get()))
 
 class Create_table(Template):
     def __init__(self):
@@ -193,25 +165,54 @@ class Create_table(Template):
         btn_ct.bind('<Button-1>', lambda event: self.view.cr_tb(
             self.entry_name.get(), self.entry_structure.get(),))
 
-class Connect(Template):
+class Add_book(Template):
     def __init__(self):
         super().__init__()
-        self.init_create_table()
+        self.init_add_book()
         self.view = app
 
-    def init_create_table(self):
-        self.title('Create table')
+    def init_add_book(self):
+            self.title('Add book')
 
-        label_name = tk.Label(self, text='Name:')
-        label_name.place(x=50, y=20)
+            label_title = tk.Label(self, text='Title:')
+            label_title.place(x=50, y=20)
 
-        self.entry_name = ttk.Entry(self)
-        self.entry_name.place(x=200, y=20)
+            label_writing_year = tk.Label(self, text='Writing year:')
+            label_writing_year.place(x=50, y=45)
 
-        btn_connect = ttk.Button(self, text='Connect with db')
-        btn_connect.place(x=220, y=170)
-        btn_connect.bind('<Button-1>', lambda event: self.view.conn(
-            self.entry_name.get()))
+            label_author_name = tk.Label(self, text='Author Name:')
+            label_author_name.place(x=50, y=70)
+
+            label_author_surname = tk.Label(self, text='Author Surname:')
+            label_author_surname.place(x=50, y=95)
+
+            label_num_book = tk.Label(self, text='Book number:')
+            label_num_book.place(x=50, y=120)
+
+
+            self.entry_title = ttk.Entry(self)
+            self.entry_title.place(x=200, y=20)
+
+            self.entry_writing_year = ttk.Entry(self)
+            self.entry_writing_year.place(x=200, y=45)
+
+            self.entry_author_name = ttk.Entry(self)
+            self.entry_author_name.place(x=200, y=70)
+
+            self.entry_author_surname = ttk.Entry(self)
+            self.entry_author_surname.place(x=200, y=95)
+
+            self.entry_num_book = ttk.Entry(self)
+            self.entry_num_book.place(x=200, y=120)
+
+
+            btn_ct = ttk.Button(self, text='Add book')
+            btn_ct.place(x=220, y=170)
+            btn_ct.bind('<Button-1>', lambda event: self.view.add_b(
+                self.entry_title.get(), self.entry_writing_year.get(),
+                self.entry_author_name.get(), self.entry_author_surname.get(),
+                self.entry_num_book.get(), ))
+
 
 class DB:
     def __init__(self):
@@ -241,7 +242,7 @@ class DB:
         self.con = psycopg2.connect(
             user="postgres",
             database=name,
-            password="22001",
+            password="12345",
             host="127.0.0.1",
             port="5432"
         )
@@ -258,18 +259,11 @@ class DB:
         print("Table created")
         self.con.commit()
 
-    def insert_data(self, description, costs, total):
-        self.c.execute('''INSERT INTO finance(description, costs, total) VALUES (?, ?, ?)''',
-                       (description, costs, total))
-        self.conn.commit()
-
+    def query_add_book(self, title, writing_year, author_name, author_surname, num_book):
+        pass
 
 if __name__ == "__main__":
-    '''con.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
-    cur = con.cursor()
-    sqlCreateDatabase = "create database Library;"
-    cur.execute(sqlCreateDatabase)
-    con.commit() '''
+
     root = tk.Tk()
     db = DB()
     app = Main(root)
